a9d719336fc3ff5a5d8d4bac1be09e4442b07c36
Akanksha Naik <Akanksha.naik2@auspost.com.au>
2020-03-15 01:19:22 +1100

    Upgrade spring boot and gradle to newer version

--- src/main/java/com/afterpay/Application.java
+++ src/main/java/com/afterpay/Application.java
@@ -3,18 +3,18 @@ package com.afterpay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App extends SpringBootServletInitializer {
+public class Application extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(App.class);
+        return application.sources(Application.class);
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
